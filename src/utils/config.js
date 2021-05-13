aa6b62bd275e9cd5f358b06bfcfd54950b07a18a
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2021-05-13 11:22:39 +0300

    refactoring

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const PIECES_TYPES = ["I", "O", "L", "J", "S", "Z", "T"];
+// Стартовые номера ячеек игровой области, в которые устанавливается фигура
 export const BASE_NUM = [3, 4, 5, 6, 13, 14, 15, 16];
 export const CONTROL_KEYS = {
   left: ["ArrowLeft", "a", "A"],
